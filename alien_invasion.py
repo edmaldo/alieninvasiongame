59dac0a6e8512b997bbd371af53af1fe16331c14
goodorangeknight <goodorangeknight@xxx.com>
2020-06-24 08:23:26 -0400

    Bullets shoot down aliens.

--- alien_invasion.py
+++ alien_invasion.py
@@ -34,6 +34,7 @@ class AlienInvasion:
 			self._check_events()
 			self.ship.update()
 			self._update_bullets()
+			self._update_aliens()
 			self._update_screen()
 
 	def _check_events(self):
@@ -80,6 +81,19 @@ class AlienInvasion:
 			if bullet.rect.bottom <= 0:
 				self.bullets.remove(bullet)
 
+		# Check for any bullets that have hit aliens.
+		# If so, get rid of the bullet and the alien.
+		collisions = pygame.sprite.groupcollide(
+			self.bullets, self.aliens, True, True)
+
+	def _update_aliens(self):
+		""""
+		Check if the fleet is at an edge,
+			then update the position of all aliens in the fleet.
+		"""
+		self._check_fleet_edges()
+		self.aliens.update()
+
 	def _create_fleet(self):
 		"""Create the fleet of aliens."""
 		alien = Alien(self)
@@ -106,6 +120,19 @@ class AlienInvasion:
 		alien.rect.x = alien.x
 		alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
 		self.aliens.add(alien)
+
+	def _check_fleet_edges(self):
+		"""Respond appropriately if any aliens have reached an edge."""
+		for alien in self.aliens.sprites():
+			if alien.check_edges():
+				self._change_fleet_direction()
+				break
+
+	def _change_fleet_direction(self):
+		"""Drop the entire fleet and change the fleet's direction."""
+		for alien in self.aliens.sprites():
+			alien.rect.y += self.settings.fleet_drop_speed
+		self.settings.fleet_direction *= -1
 
 	def _update_screen(self):
 		"""Update images on the screen, and flip to the new screen."""
--- settings.py
+++ settings.py
@@ -4,13 +4,17 @@ class Settings:
 	def __init__(self):
 		"""Initialize the game's settings."""
 		
+		#Alien settings
+		self.alien_speed = 1.0
+		self.fleet_drop_speed = 1.0
+		self.fleet_direction = 1
+
 		#Bullet settings
 		self.bullet_speed = 1.0
 		self.bullet_width = 3
 		self.bullet_height = 15
 		self.bullet_color = (220, 25, 25)
 		self.bullets_allowed = 3
-
 
 
 		#Screen settings
